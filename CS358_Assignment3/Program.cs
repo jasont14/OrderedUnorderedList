29f79ac2e8187c725fe55a976845e33eb8eaa78c
Jason Thatcher <contact@jasonthatcher.com>
2018-02-11 20:37:14 -0600

    added initial methods and static tests

--- CS358_Assignment3/Program.cs
+++ CS358_Assignment3/Program.cs
@@ -10,24 +10,50 @@ namespace OrderUnorderList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-
             UnorderedArrayList u = new UnorderedArrayList();
-            u.print();
-            int var = 5;
-            u.insert(ref var);
-            var = 12;
-            u.insert(ref var);
-            var = 2;
-            u.insert(ref var);
-            var = 29;
-            u.insert(ref var);
-            u.print();
-            var = 5;
-            u.remove(ref var);
-            u.print();
+            Console.WriteLine("INITIAL LIST. LIST SHOULD BE EMPTY");
+            u.Print();
 
+            int[] values = { 1, 3, 5, 7, 5, 9, 3, 2, 4, 6, 8, 11, 14, 13, 2, 15, 12, 14, 2, 3, 7, 9, 6, 12, 2 };
+            u.InsertValues(ref values);
+
+            Console.WriteLine("\nLIST LOADED. LIST SHOULD NOT BE EMPTY. LIST SHOULD HAVE 25 VALUES.");
+            u.Print();
+            u.PrintListInformation();
+
+            Console.WriteLine("\nCALL REMOVE(3).  WILL REMOVE FIRST VALUE OF 3 FROM LIST(POSITION 2) BY REPLACING WITH END OF LIST VALUE (2) AND SHORTENING LIST. LENGTH SHOULD BE 24.  ");
+            int value = 3;
+            u.Remove(ref value);
+            u.Print();
+            u.PrintListInformation();
+
+            Console.WriteLine("\nCALL REMOVEALL(3).  WILL REMOVE ALL VALUES OF 3 FROM LIST (POSITIONS 7 & POSITION 18). LENGTH SHOULD BE 22. ");
+            u.RemoveAll(ref value);
+            u.Print(); 
+            u.PrintListInformation();
             Console.ReadKey();
+        }
+
+        
+
+        public static void TestRemove()
+        {
+
+        }
+
+        public static void TestInsertionSort()
+        {
+
+        }
+
+        public static void TestRemoveAll()
+        {
+
+        }
+
+        public static void TestMinMax()
+        {
+
         }
     }
 }
--- CS358_Assignment3/UnorderedArrayList.cs
+++ CS358_Assignment3/UnorderedArrayList.cs
@@ -6,6 +6,7 @@ namespace OrderUnorderList
     {
         protected int[] list;
         protected int next;
+        protected int length;
 
         public UnorderedArrayList()
         {
@@ -14,39 +15,106 @@ namespace OrderUnorderList
         }
 
 
-        public void insert(ref int item)
+        public void Insert(ref int item)
         {
             list[next] = item;
             next++;
         }
+
+        public void InsertValues(ref int[] items)
+        {
+            for(int i=0; i<items.Length; i++)
+            {
+                list[i] = items[i];
+                next++;
+            }
+        
+        }
     
-        public void remove(ref int item)
+        public void Remove(ref int item)
         {
             if (next == 0)
             {
             }
             else
-            {
-            //find value, if it exists
+            {           
                 for (int i = 0; i < next; i++)
                 {
-                    if (item.Equals(list[i]))
-                    {
-                        for (int j = i; j < next; j++) list[j] = list[j + 1];
+                    if (item.Equals(list[i])) //find value, if it exists
+                    {                        
+                        list[i] = list[next - 1]; //Replace item with last item
+                        list[next - 1] = 0;    //Remove last item                    
                         next--;
                         break;
                     }
                 }
             }
+        
+        }
+               
+        public void RemoveAll(ref int item)
+        {
+            for(int i=0; i<next; i++)
+            {
+                if (item.Equals(list[i]))
+                {
+                    list[i] = list[next - 1]; //Replace item with last item
+                    list[next - 1] = 0;    //Remove last item                    
+                    next--;
+                }
+            }
+
         }
 
-        public void print()
+        public int Min()
         {
+            int result = list[0];
+
+            //loop through list and set lowest
+
+            for(int i = 0; i<next; i++)
+            {
+                if (list[i] < result)
+                {
+                    result = list[i];
+                }
+            }
+
+            return result;
+        }
+
+        public int Max()
+        {
+            int result = list[0];
+
+            //loop through list and set lowest
+
             for (int i = 0; i < next; i++)
             {
-                Console.WriteLine(list[i]);
+                if (list[i] > result)
+                {
+                    result = list[i];
+                }
             }
+
+            return result;
+        }
+
+        public void Print()
+        {
+            Console.Write("LIST: ");
+
+            for (int i = 0; i < next; i++)
+            {
+                Console.Write(" " + list[i]);
+            }
+
             Console.WriteLine();
+        }
+
+        public void PrintListInformation()
+        {
+            Console.WriteLine("\nLIST LENGTH:{0} \n\rFIRST ENTRY VALUE: {1} \n\rLAST ENTRY VALUE: {2} \n\rMIN VALUE: {3} \n\rMAX VALUE: {4}", next.ToString(), list[0].ToString(), list[next - 1].ToString(), Min().ToString(), Max().ToString());
         }
     }
 
