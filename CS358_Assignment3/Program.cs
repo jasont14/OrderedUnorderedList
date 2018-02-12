a091d98c86a9b51885347e7582ce8368ed6a34c6
Jason Thatcher <contact@jasonthatcher.com>
2018-02-11 18:59:34 -0600

    init commit

--- CS358_Assignment3/Program.cs
+++ CS358_Assignment3/Program.cs
@@ -10,6 +10,24 @@ namespace CS358_Assignment3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+
+            UnorderedArrayList u = new UnorderedArrayList();
+            u.print();
+            int var = 5;
+            u.insert(ref var);
+            var = 12;
+            u.insert(ref var);
+            var = 2;
+            u.insert(ref var);
+            var = 29;
+            u.insert(ref var);
+            u.print();
+            var = 5;
+            u.remove(ref var);
+            u.print();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CS358_Assignment3/UnorderedArrayList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CS358_Assignment3
+{
+    public class UnorderedArrayList
+    {
+        protected int[] list;
+        protected int next;
+
+        public UnorderedArrayList()
+        {
+            list = new int[100];
+            next = 0;
+        }
+
+
+        public void insert(ref int item)
+        {
+            list[next] = item;
+            next++;
+        }
+    
+        public void remove(ref int item)
+        {
+            if (next == 0)
+            {
+            }
+            else
+            {
+            //find value, if it exists
+                for (int i = 0; i < next; i++)
+                {
+                    if (item.Equals(list[i]))
+                    {
+                        for (int j = i; j < next; j++) list[j] = list[j + 1];
+                        next--;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < next; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+}
